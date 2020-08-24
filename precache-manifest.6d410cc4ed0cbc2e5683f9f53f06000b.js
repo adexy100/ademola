f16aa29c8a1e2e1f6bf81248106dbb5b7b93330c
unknown <adexy100@gmail.com>
2020-08-24 11:08:37 -1200

    Updates

---
+++ precache-manifest.6d410cc4ed0cbc2e5683f9f53f06000b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fea53e780d024763262f6fb56f499b7",
+    "url": "/ademola/index.html"
+  },
+  {
+    "revision": "9e99f0a25c73b63d0dfc",
+    "url": "/ademola/static/css/2.dec34a04.chunk.css"
+  },
+  {
+    "revision": "36a189085c5c3d6acb05",
+    "url": "/ademola/static/css/main.78bad890.chunk.css"
+  },
+  {
+    "revision": "9e99f0a25c73b63d0dfc",
+    "url": "/ademola/static/js/2.b3d1d107.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/ademola/static/js/2.b3d1d107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a189085c5c3d6acb05",
+    "url": "/ademola/static/js/main.5d185aa8.chunk.js"
+  },
+  {
+    "revision": "4c8a29ed435c147677c6",
+    "url": "/ademola/static/js/runtime-main.f737df72.js"
+  },
+  {
+    "revision": "87d2350754f10a9c1c8887acd01daf0b",
+    "url": "/ademola/static/media/AdemolaCV.87d23507.pdf"
+  },
+  {
+    "revision": "6195e8d208a6015a38344c40c9ebdce0",
+    "url": "/ademola/static/media/Face-Detect-Capture.6195e8d2.PNG"
+  },
+  {
+    "revision": "f12fc94c1f9aaa819e20abd6c15aae0f",
+    "url": "/ademola/static/media/ProfilePic2.f12fc94c.jpeg"
+  },
+  {
+    "revision": "453d09d53fc852063de6208b14c86cd8",
+    "url": "/ademola/static/media/Robofriends-capture.453d09d5.png"
+  },
+  {
+    "revision": "515f7e6d67ae4e819713ef4b44ac00a5",
+    "url": "/ademola/static/media/bills-tracker-capture.515f7e6d.PNG"
+  },
+  {
+    "revision": "b77b0218b772a07bed5ae23d9eea5674",
+    "url": "/ademola/static/media/pixel-art-capture.b77b0218.PNG"
+  },
+  {
+    "revision": "494c3a9144ceee2d1ece127d3929ea22",
+    "url": "/ademola/static/media/profile-photo-2.494c3a91.jpeg"
+  },
+  {
+    "revision": "61e77e8b4b3eb557cbe4a0a15a1dee6d",
+    "url": "/ademola/static/media/project-1.61e77e8b.png"
+  },
+  {
+    "revision": "0200e3a2ed9f6d8eb132f0ab6f4cc763",
+    "url": "/ademola/static/media/project-2.0200e3a2.png"
+  },
+  {
+    "revision": "4e30dd1bffa8d80c77e66a50407f1f6f",
+    "url": "/ademola/static/media/project-3.4e30dd1b.png"
+  },
+  {
+    "revision": "d08702b2935f0990e841ab10236a2d70",
+    "url": "/ademola/static/media/project-4.d08702b2.png"
+  },
+  {
+    "revision": "92f4e7b64b772a8d866e2e561ebe3a8e",
+    "url": "/ademola/static/media/project-5.92f4e7b6.png"
+  },
+  {
+    "revision": "632058292c75fe586053b6f4277edf4e",
+    "url": "/ademola/static/media/project-6.63205829.png"
+  },
+  {
+    "revision": "2fe6846b66e8411058a421798d2b8484",
+    "url": "/ademola/static/media/project-7.2fe6846b.png"
+  },
+  {
+    "revision": "89f63daad9fe08ccead9316f07925687",
+    "url": "/ademola/static/media/store-checkout-capture.89f63daa.PNG"
+  },
+  {
+    "revision": "c512c8b89d53231d2a52a0b002bded8a",
+    "url": "/ademola/static/media/the_weather_app_capture.c512c8b8.PNG"
+  }
+]);
